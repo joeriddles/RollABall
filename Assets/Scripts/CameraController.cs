0cc288394afe0d42f5630d6c3e92f11ed9249265
joeriddles <joeriddles10@gmail.com>
2019-05-20 12:07:10 -0700

    Add Unity project files.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset; // camera offset
+
+    void Start()
+    {
+		offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate() // runs every frame like Update(). Is guaranteed to run after all other objects.
+    {
+		transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rigidbody;
+	private int count;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+	// Called before physics calculations
+	void FixedUpdate()
+	{
+		// Input.	: get input from player
+		// GetAxis	: controlled by keys on keyboard
+		// Use this input to add forces to player.
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rigidbody.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pickup")) // is the object we collided with a Pickup prefab?
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 7) // use number of Pickups here
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
